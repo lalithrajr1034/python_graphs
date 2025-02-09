d5fb6eab514fe0dcd083e3f26c4e68b958c51ba2
lalithrajr1034 <placement1034@gmail.com>
2025-02-10 00:18:30 +0530

    1aupdating features for bar graph

--- program1.py
+++ program1.py
@@ -3,5 +3,7 @@ product_A_sales = [100, 120, 130, 150, 160, 180]
 product_B_sales = [80, 100, 90, 110, 120, 130]
 plt.bar(product_A_sales, product_B_sales)
 plt.title('Recommended Time Allocation for Academic and Career Preparation')
+plt.xlabel('Product A Sales')
+plt.ylabel('Product B Sales')
 plt.legend()
 plt.show()
